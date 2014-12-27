744a5f79e111d70a1aebd00dd473047d8dcafd03
Jakub Mandula <jakub.aludnam@gmail.com>
2014-12-27 20:42:07 +0100

    add coverage

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp'),
   uglify = require('gulp-uglify'),
+
+  istanbul = require("gulp-istanbul"),
+  coveralls = require('gulp-coveralls'),
   mocha = require('gulp-mocha');
 
 var sourcePath = {
@@ -12,9 +15,20 @@ gulp.task('build', function() {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('test', function() {
-  return gulp.src("test/*.js")
-    .pipe(mocha());
+gulp.task('test', function(cb) {
+  gulp.src(sourcePath.source)
+    .pipe(istanbul()) // Covering files
+    .on('finish', function() {
+      gulp.src(['test/*.js'])
+        .pipe(mocha())
+        .pipe(istanbul.writeReports()) // Creating the reports after tests ran
+        .on('end', cb);
+    });
+});
+
+gulp.task('coverage', function() {
+  gulp.src("coverage/lcov.info")
+    .pipe(coveralls());
 });
 
 gulp.task('default', ['build', 'test']);
